3cab6b55b27ccb6e99c27280e1fb1b81fafc03b1
dushyant2901 <74450982+dushyant2901@users.noreply.github.com>
2021-09-22 20:42:24 +0530

    added data

---
+++ src/components/Data.js
@@ -0,0 +1,25 @@
+
+
+
+const Data = [{
+  id:1,
+    title: 'abcd',
+    description: 'twyw wyww eeeeeeeee  yw'
+},
+    {id:2,
+      title: 'abcd1',
+    description: 'twyw wyww eeeeeeeee  yw'
+},
+    { id:3,  title: 'abcd2',
+    description: 'twyw wyww eeeeeeeee  yw'
+},
+    {id:4, title: 'abcd3',
+    description: 'twyw wyww eeeeeeeee  yw'
+},
+
+
+
+
+]
+
+export default Data

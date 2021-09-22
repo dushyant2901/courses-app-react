59430ff2fbe415fc1470db0db9944f4df0e55958
dushyant2901 <74450982+dushyant2901@users.noreply.github.com>
2021-09-22 20:42:44 +0530

    added home component

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Jumbotron} from 'reactstrap'
+
+function Home() {
+    return (
+        <div>
+            <Jumbotron>
+         Description
+            </Jumbotron>
+        </div>
+    )
+}
+
+export default Home
